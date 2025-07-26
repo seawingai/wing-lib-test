61526589a1d1cc4fe35a25e6ba2c8dc7f9bfce82
rafeyhusain <rafey.husain@gmail.com>
2025-07-26 16:44:33 +0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,6 @@
+import { WingDate, Recurrence } from '@seawingai/wingdate';
+
+// Create a date instance
+const date = new WingDate('2024-01-15T10:30:00Z');
+
+console.log(date.ISO);
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const wingdate_1 = require("@seawingai/wingdate");
+// Create a date instance
+const date = new wingdate_1.WingDate('2024-01-15T10:30:00Z');
+console.log(date.ISO);
